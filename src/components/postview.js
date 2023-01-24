fe73a2000c40be744f8e7b1a2b9879d4d5c81959
ChandiniShivanna <120159560+ChandiniShivanna@users.noreply.github.com>
2023-01-24 18:07:13 +0530

    Update postview.js

--- src/components/postview.js
+++ src/components/postview.js
@@ -12,7 +12,7 @@ export default function Postview() {
     const [Indicators, setIndicators] = useState(true);
 
     useEffect(() => {
-        fetch("http://localhost:3600/posts")
+        fetch("https://instaclone-backend-2023-4gv1.onrender.com")
             .then(res => { return res.json() })
             .then(data => {
                 console.log(data);
